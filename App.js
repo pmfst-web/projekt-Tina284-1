cef87bc3d9f168c0ac91ae0a21402efa23141d35
Tina284 <77157761+Tina284@users.noreply.github.com>
2023-06-03 11:13:37 +0200

    v01
    v01

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { MaterialIcons } from '@expo/vector-icons';
 
-export default function App() {
+import PocetniEkran from './screens/PocetniEkran';
+import ReceptiEkran from './screens/ReceptiEkran';
+import UnosEkran from './screens/UnosEkran';
+import Boje from './constants/Boje';
+const Stack = createNativeStackNavigator();
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator()
+
+
+
+const tabImenik = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="svi" component={PopisEkran} initialParams={['svi']} />
+      <Tab.Screen name="favoriti" component={PopisEkran} initialParams={['favoriti']}/>
+    </Tab.Navigator>
+  );
+};
+
+function App() {
+  return (
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: Boje.Naglasak1,
+            },
+            headerTintColor: Boje.Primarna,
+          }}>
+          <Stack.Screen
+            name="Naslovna"
+            component={PocetniEkran}
+            options={{
+              title: 'Moja kuharica',
+            }}
+          />
+          <Stack.Screen
+            name="Popis"
+            component={tabImenik}
+            options={({ route, navigation }) => {
+              return {
+                headerRight: () => {
+                  return (
+                    <TouchableOpacity
+                      onPress={() => navigation.navigate('Unos')}>
+                      <View>
+                        <MaterialIcons
+                          name="note-add"
+                          size={26}
+                          color={Boje.Primarna}
+                        />
+                      </View>
+                    </TouchableOpacity>
+                  );
+                },
+              };
+            }}
+          />
+          
+          <Stack.Screen name="Unos" component={UnosEkran} />
+          <Stack.Screen name="Recepti" component={ReceptiEkran} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
