6e5fd92273857dd131a8585ede8b0a9dfab63b21
Tina284 <77157761+Tina284@users.noreply.github.com>
2023-06-16 19:10:25 +0200

    v03
    v03 - dodavanje modela

---
+++ models/recept.js
@@ -0,0 +1,10 @@
+class Recept {
+  constructor(id, naslov, tekst, vrsta){
+    this.id = id;
+    this.naslov = naslov;
+    this.tekst = tekst;
+    this.vrsta = vrsta;
+  }
+}
+
+export default Recept
